731d811154c0831d57620f0f7e647bbc9912a6f9
AHEDeveloper <esmailiamirhosein45@gmail.com>
2025-11-29 12:20:46 +0330

    create app Admin & create header component

---
+++ resources/views/layouts/admin/link.blade.php
@@ -0,0 +1,15 @@
+<link rel="icon" type="image/x-icon" href="/admin/src/assets/img/favicon.ico"/>
+<link href="/admin/layouts/vertical-dark-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
+<link href="/admin/layouts/vertical-dark-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
+<script src="/admin/layouts/vertical-dark-menu/loader.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+<link href="/admin/src/bootstrap/css/bootstrap.rtl.min.css" rel="stylesheet" type="text/css" />
+<link href="/admin/layouts/vertical-dark-menu/css/light/plugins.css" rel="stylesheet" type="text/css" />
+<link href="/admin/layouts/vertical-dark-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />
+
+<link href="/admin/src/plugins/src/apex/apexcharts.css" rel="stylesheet" type="text/css">
+<link href="/admin/src/assets/css/light/components/list-group.css" rel="stylesheet" type="text/css">
+<link href="/admin/src/assets/css/light/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+
+<link href="/admin/src/assets/css/dark/components/list-group.css" rel="stylesheet" type="text/css">
+<link href="/admin/src/assets/css/dark/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
